746d9a5509118c041df56459cc22ceb0753f372b
Son Beomsoo <k1a11220@naver.com>
2022-11-22 00:14:35 +0900

    feat: getNFT 구현

---
+++ pages/api/get-nft.ts
@@ -0,0 +1,24 @@
+import { EvmChain } from '@moralisweb3/evm-utils'
+import Moralis from 'moralis'
+
+export default async function getNFT(req, res) {
+  console.log(req.body)
+  const address = JSON.parse(req.body)
+
+  await Moralis.start({
+    apiKey: process.env.MORALIS_API_KEY
+    // ...and any other configuration
+  })
+
+  const chain = EvmChain.POLYGON
+  try {
+    const response = await Moralis.EvmApi.nft.getWalletNFTCollections({
+      address,
+      chain
+    })
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(400).json({ error })
+    console.error(error)
+  }
+}
--- pages/user.tsx
+++ pages/user.tsx
@@ -1,12 +1,25 @@
+import axios from 'axios'
 import { getSession, signOut } from 'next-auth/react'
 
 // gets a prop from getServerSideProps
 function User({ user }) {
+  const handleGetNFT = async () => {
+    const { data } = await axios.post('/api/get-nft', user.address, {
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+
+    const response = data.response
+
+    console.log(data)
+  }
   return (
     <div>
       <h4>User session:</h4>
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <button onClick={() => signOut({ redirect: '/signin' })}>Sign out</button>
+      <button onClick={() => handleGetNFT()}>get NFT List</button>
     </div>
   )
 }
