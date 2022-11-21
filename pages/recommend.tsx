38ae7a19120137d43a99591fac5432da918005c6
Son Beomsoo <k1a11220@naver.com>
2022-11-21 16:00:39 +0900

    feat: 도서검색 스타일 추가

--- pages/recommend.tsx
+++ pages/recommend.tsx
@@ -1,7 +1,112 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
+import styled from '@emotion/styled'
+
 import { bookSearch } from './api/book-search'
+
+const Container = styled.div`
+  background: url('https://i.imgur.com/7XaIf8n.png') center center;
+  background-repeat: no-repeat;
+  background-size: 100vw;
+  display: flex;
+  align-items: center;
+  height: 100vh;
+
+  & > * {
+    color: var(--fg-color);
+  }
+
+  & > div {
+    display: flex;
+    background-color: #fff;
+    width: 640px;
+    margin: 0 auto;
+    flex-direction: column;
+    margin: 0 auto;
+    align-items: center;
+    justify-content: start;
+    height: 480px;
+    overflow: scroll;
+    padding: 0 16px 16px 16px;
+    border-radius: 16px;
+  }
+`
+
+const BookContainer = styled.div`
+  display: flex;
+  align-items: start;
+  justify-content: start;
+  border-radius: 16px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  & img {
+    border-radius: 8px;
+    max-width: 80px;
+    margin-right: 24px;
+  }
+
+  &:hover {
+    background-color: #f3f3f3;
+    scale: 0.98;
+    padding: 16px;
+  }
+`
+
+const BookDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  & p {
+    margin: 0;
+  }
+
+  & > p {
+    font-size: 18px;
+    font-weight: 500;
+  }
+
+  & span {
+    font-size: 14px;
+    color: #727272;
+  }
+`
+
+const SearchBar = styled.input`
+  width: 100%;
+  font-size: 18px;
+  height: 72px;
+  padding-left: 16px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const Icon = styled.svg`
+  width: 24px;
+`
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  background-color: #fff;
+  border-bottom: 1px solid #e9ebee;
+`
+
+const BookList = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 24px;
+  margin-top: 24px;
+`
 
 const recommend = () => {
   const [query, setQuery] = useState('')
@@ -22,20 +127,46 @@ const recommend = () => {
 
   console.log(data)
   return (
-    <div>
-      <input value={query} onChange={(e) => setQuery(e.target.value)}></input>
+    <Container>
       <div>
-        {data.map((post) => (
-          <>
-            {post.authors.map((data) => (
-              <p>{data}</p>
-            ))}
-            <img src={post.thumbnail} layout='fill' />
-            <p>{post.title}</p>
-          </>
-        ))}
+        <SearchBarContainer>
+          <Icon
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+            />
+          </Icon>
+          <SearchBar
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          ></SearchBar>
+        </SearchBarContainer>
+
+        <BookList>
+          {data.map((post) => (
+            <BookContainer>
+              <img src={post.thumbnail} layout='fill' />
+              <BookDetails>
+                <p>{post.title}</p>
+                <div>
+                  {post.authors.map((data) => (
+                    <span>{data}</span>
+                  ))}
+                </div>
+              </BookDetails>
+            </BookContainer>
+          ))}
+        </BookList>
       </div>
-    </div>
+    </Container>
   )
 }
 
