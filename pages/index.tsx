bae0e4272fec44e2804f34a1426d2d2e67b43439
Son Beomsoo <k1a11220@naver.com>
2022-12-02 13:05:52 +0900

    fix: https 에러 해결

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,17 @@ import Story from "@components/home/Story";
 import Team from "@components/home/Team";
 import Title from "@components/home/Title";
 import { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <main>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <Title />
       <Story />
       <Roadmap />
